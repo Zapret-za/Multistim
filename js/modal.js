9e5ae270a6ad4fe7e5a96285ff23b11b4b70068c
Zapret-za <109032826+Zapret-za@users.noreply.github.com>
2023-04-26 13:26:48 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,26 +1,23 @@
-var modal = document.getElementById("myModal");
-var btn1 = document.getElementById("myBtn1");
-var btn2 = document.getElementById("myBtn2");
-var btn3 = document.getElementById("myBtn3");
-var span = document.getElementsByClassName("close")[0];
-btn1.onclick = function() {
-  modal.style.display = "block";
-  document.documentElement.classList.add('lock1');
-}
-btn2.onclick = function() {
-  modal.style.display = "block";
-  document.documentElement.classList.add('lock1');
-}
-btn3.onclick = function() {
-  modal.style.display = "block";
-  document.documentElement.classList.add('lock1');
-}
-span.onclick = function() {
-  modal.style.display = "none";
-  document.documentElement.classList.remove('lock1');
-}
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+var modalOpenButtons = document.querySelectorAll('.show__modal');
+modalOpenButtons.forEach(function(button) {
+button.addEventListener('click', function(event) {
+event.preventDefault();
+var modal = document.querySelector('.modal');
+modal.style.display = 'block';
+document.documentElement.classList.toggle('lock1');
+});
+});
+
+var modalCloseButtons = document.querySelectorAll('.close__modal');
+modalCloseButtons.forEach(function(button) {
+button.addEventListener('click', function(event) {
+event.preventDefault();
+closeModal();
+});
+});
+
+function closeModal() {
+var modal = document.querySelector('.modal');
+modal.style.display = 'none';
+document.documentElement.classList.remove('lock1');
 }
